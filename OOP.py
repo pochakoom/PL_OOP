db5e43442fbfafee11fe666d2f80b60334f2c0bf
Pochakoom_s <116154880+pochakoom@users.noreply.github.com>
2024-09-25 18:18:49 +0700

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,38 +1,44 @@
 class Animal:
+    #Constructor
     def __init__(self):
         print("create animal")
         
     def sound(self):
         print("animal makes a sound")
         
+    #Destructor
     def __del__(self):
         print("delete animal")
 
-class Dog(Animal):
+class Dog(Animal): # Dog สืบทอดจาก Animal (Inheritance)
+    #Constructor
     def __init__(self):
         print("dog")
         
     def sound(self):
         print("dog barks")
         
+    #Destructor
     def __del__(self):
         print("delete dog")
         
-class Cat(Animal):
+class Cat(Animal): # Cat สืบทอดจาก Animal (Inheritance)
+    #Constructor
     def __init__(self):
         print("cat")
         
     def sound(self):
         print("cat meows")
         
+    #Destructor
     def __del__(self):
         print("delete cat")
 
 if __name__ == "__main__":
-    a = Animal()  
-    d = Dog()     
-    c = Cat()    
+    a = Animal()  # สร้างออบเจกต์จากคลาส Animal
+    d = Dog()     # สร้างออบเจกต์จากคลาส Dog
+    c = Cat()     # สร้างออบเจกต์จากคลาส Cat
     
-    a.sound()     # ส่ง message เพื่อเรียกเมธอด sound() ของออบเจกต์ a
-    d.sound()     # ส่ง message เพื่อเรียกเมธอด sound() ของออบเจกต์ d
-    c.sound()     # ส่ง message เพื่อเรียกเมธอด sound() ของออบเจกต์ c
+    a.sound()     # ส่ง message เพื่อเรียกเมธอด sound() ของออบเจกต์ a 
+    d.sound()     # ส่ง message เพื่อเรียกเมธอด sound() ของออบเจกต์ d ซึ่ง override จาก Animal (Polymorphism)
+    c.sound()     # ส่ง message เพื่อเรียกเมธอด sound() ของออบเจกต์ c ซึ่ง override จาก Animal (Polymorphism)

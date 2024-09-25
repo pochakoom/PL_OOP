bba222c6d098ffce9b7f662aef5249df88d0c5a2
Pochakoom_s <116154880+pochakoom@users.noreply.github.com>
2024-09-25 17:12:30 +0700

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,38 +1,38 @@
 class Animal:
-    def _init_(self):
+    def __init__(self):
         print("create animal")
         
     def sound(self):
         print("animal makes a sound")
         
-    def _del_(self):
+    def __del__(self):
         print("delete animal")
 
 class Dog(Animal):
-    def _init_(self):
+    def __init__(self):
         print("dog")
         
     def sound(self):
         print("dog barks")
         
-    def _del_(self):
+    def __del__(self):
         print("delete dog")
         
 class Cat(Animal):
-    def _init_(self):
+    def __init__(self):
         print("cat")
         
     def sound(self):
         print("cat meows")
         
-    def _del_(self):
+    def __del__(self):
         print("delete cat")
 
 if __name__ == "__main__":
-    a = Animal()
-    d = Dog()
-    c = Cat()
+    a = Animal()  
+    d = Dog()     
+    c = Cat()    
     
-    a.sound()
-    d.sound()
-    c.sound()
+    a.sound()     
+    d.sound()     
+    c.sound()     at
